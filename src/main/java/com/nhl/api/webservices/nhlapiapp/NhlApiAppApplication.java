cfdf473c0c898b2f175edb71f0d79246863669b8
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-06-18 01:31:49 -0400

    added jwt. adding api auth routes and connecting to react front next

---
+++ src/main/java/com/nhl/api/webservices/nhlapiapp/NhlApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.nhl.api.webservices.nhlapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhl/api/webservices/nhlapiapp/NhlApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhl.api.webservices.nhlapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
