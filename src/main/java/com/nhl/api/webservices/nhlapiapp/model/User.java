12896f9bc44191a583981439cce4b9454c7c3bb9
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-07-01 11:57:51 -0400

    user db and model created

--- src/main/java/com/nhl/api/webservices/nhlapiapp/model/User.java
+++ src/main/java/com/nhl/api/webservices/nhlapiapp/model/User.java
@@ -1,5 +1,74 @@
 package com.nhl.api.webservices.nhlapiapp.model;
 
+//    id INT AUTO_INCREMENT PRIMARY KEY,
+//            name VARCHAR(50),
+//            username varchar(50),
+//            email varchar(255),
+//            password varchar(255),
+//            favorite_team varchar(100)
+
+
+import javax.persistence.*;
+import java.sql.Struct;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "favorite_team")
+    private String favoriteTeam;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFavoriteTeam() {
+        return favoriteTeam;
+    }
+
+    public void setFavoriteTeam(String favoriteTeam) {
+        this.favoriteTeam = favoriteTeam;
+    }
 }
