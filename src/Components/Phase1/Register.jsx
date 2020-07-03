6595dfacb68df52118f0b63d0305706f35883870
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-07-03 15:27:36 -0400

    registration setup

---
+++ src/Components/Phase1/Register.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import AuthenticationService from "./AuthenticationService";
+
+class Login extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+            favoriteTeam: '',
+            showSuccessMessage: false
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.registerClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name] : event.target.value
+            }
+        )
+    }
+
+    registerClicked() {
+        // AuthenticationService
+        // .executeJwtAuthenticationService(this.state.username, this.state.password)
+        // .then((response) => {
+        //     AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, response.data.token)
+        //     this.props.history.push(`/welcome/${this.state.username}`)
+        // }).catch( () => {
+        //     this.setState({showSuccessMessage:false})
+        //     this.setState({hasLoginFailed:true})
+        // })
+        
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+            <h1>Login</h1>
+            <div>
+                {this.state.showSuccessMessage && <div>Registration Successful</div>}
+                {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid inputs</div>}
+                <div>
+                    Name: <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                    User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                    Email: <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                    Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    favoriteTeam: <input type="text" name="favoriteTeam" value={this.state.favoriteTeam} onChange={this.handleChange} />
+                    <button className="btn btn-success" onClick={this.loginClicked} >Register</button>
+                </div>
+            </div>
+            </div>
+        )
+    }
+
+}
+
+export default Register
