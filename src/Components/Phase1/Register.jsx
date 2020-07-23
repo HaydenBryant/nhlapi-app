365e5acc2b254393392032453d88183b2077295a
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-07-23 14:09:29 -0400

    made team dropdown better

--- src/Components/Phase1/Register.jsx
+++ src/Components/Phase1/Register.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import UserDataService from "../../api/user/UserDataService.js"
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup';
+import {teams} from '../../Constants'
 
 class Register extends Component{
     constructor(props) {
@@ -92,38 +93,6 @@ class Register extends Component{
     // }
 
     generateTeamOptions(){
-
-            const teams = ["Anaheim Ducks",
-                "Arizona Coyotes",
-                "Boston Bruins",
-                "Buffalo Sabres",
-                "Calgary Flames",
-                "Carolina Hurricanes",
-                "Chicago Blackhawks",
-                "Colorado Avalanche",
-                "Columbus Blue Jackets",
-                "Dallas Stars",
-                "Detroit Red Wings",
-                "Edmonton Oilers",
-                "Florida Panthers",
-                "Los Angeles Kings",
-                "Minnesota Wild",
-                "Montreal Canadiens",
-                "Nashville Predators",
-                "New Jersey Devils",
-                "New York Islanders",
-                "New York Rangers",
-                "Ottawa Senators",
-                "Philadelphia Flyers",
-                "Pittsburgh Penguins",
-                "San Jose Sharks",
-                "St Louis Blues",
-                "Tampa Bay Lightning",
-                "Toronto Maple Leafs",
-                "Vancouver Canucks",
-                "Vegas Golden Knights",
-                "Washington Capitals",
-            "Winnipeg Jets"]
 
             const teamElements = []
 
