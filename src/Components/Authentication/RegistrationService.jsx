b66e5758fa9284c113d6d7b27e4beb1cabd6017f
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-08-09 11:17:48 -0400

    cleanup. trying to get users favoriote team. need to authorize tokens to check

--- src/Components/Authentication/RegistrationService.jsx
+++ src/Components/Authentication/RegistrationService.jsx
@@ -1,28 +1,12 @@
 import UserDataService from '../../api/user/UserDataService'
-// import axios from 'axios'
-// import { API_URL } from '../../Constants'
 
 
 
 class RegistrationService {
 
-//register user
-//try catch
-//does user exist(backend)
-    // hashPass(password){
-    //     bcryptjs.genSalt(10, function(err, salt) {
-    //         bcryptjs.hash(password, salt, function(err, hash) {
-    //             return hash;
-    //         });
-    //     });
-    // }
-
     registerUser(user){
-        // var user = [user]
         console.log("register user")
         UserDataService.registerUser(user)
-        console.log("done")
-        // return axios.post(`${API_URL}/register`, user)
     }
 }
 
